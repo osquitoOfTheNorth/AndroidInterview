a1dfa1f4cd2be830bb4603d2d3355fe9d1751ddb
Oscar Menjivar <omenjivar@intuit.com>
2023-10-04 10:19:02 -0400

    Remove tests

--- app/src/test/java/com/example/myapplication/UtilUnitTest.kt
+++ app/src/test/java/com/example/myapplication/UtilUnitTest.kt
@@ -1,16 +1,1 @@
 package com.example.myapplication
-
-import org.junit.Test
-
-import org.junit.Assert.*
-
-class UtilUnitTest {
-    @Test
-    fun validateIsConsecutiveNumbers() {
-        assertTrue(Util.isConsecutiveNumbers(arrayOf(5, 1, 4, 3, 2)))
-        assertTrue(Util.isConsecutiveNumbers(arrayOf(-1, -2, 0)))
-        assertTrue(Util.isConsecutiveNumbers(arrayOf(5)))
-        assertFalse(Util.isConsecutiveNumbers(arrayOf(5, 1, 4, 3, 2, 8)))
-        assertFalse(Util.isConsecutiveNumbers(arrayOf(5, 6, 7, 8, 9, 9)))
-    }
-}
