2defd95ecf9517d1eaaf28fb3fef20b26735d849
Oscar Menjivar <omenjivar@intuit.com>
2023-10-04 10:06:10 -0400

    Tweak for coops

--- app/src/main/java/com/example/myapplication/Util.kt
+++ app/src/main/java/com/example/myapplication/Util.kt
@@ -10,6 +10,12 @@ object Util {
      * return True if the array can be re-arranged meeting the criteria
      */
     fun isConsecutiveNumbers (int: Array<Int>) : Boolean {
+        int.sort()
+        int.forEachIndexed { index, i ->
+            if(index < int.size - 1 && i + 1 != int[index + 1]) {
+                return false
+            }
+        }
         return true
     }
 }
--- app/src/test/java/com/example/myapplication/UtilUnitTest.kt
+++ app/src/test/java/com/example/myapplication/UtilUnitTest.kt
@@ -8,6 +8,8 @@ class UtilUnitTest {
     @Test
     fun validateIsConsecutiveNumbers() {
         assertTrue(Util.isConsecutiveNumbers(arrayOf(5, 1, 4, 3, 2)))
+        assertTrue(Util.isConsecutiveNumbers(arrayOf(-1, -2, 0)))
+        assertTrue(Util.isConsecutiveNumbers(arrayOf(5)))
         assertFalse(Util.isConsecutiveNumbers(arrayOf(5, 1, 4, 3, 2, 8)))
         assertFalse(Util.isConsecutiveNumbers(arrayOf(5, 6, 7, 8, 9, 9)))
     }
